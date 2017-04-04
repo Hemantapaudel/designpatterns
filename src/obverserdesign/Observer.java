59ddcf8526c984bdcd0408ad73de127018750391
Hemantapaudel <hemantapaudel05@gmail.com>
2017-04-04 23:11:39 +0530

    adding observer defination file

---
+++ src/obverserdesign/Observer.java
@@ -0,0 +1,5 @@
+package obverserdesign;
+
+public interface Observer {
+	public void update(double ibmPrice, double aaplPrice, double googPrice);
+}
---
+++ src/obverserdesign/Subject.java
@@ -0,0 +1,11 @@
+package obverserdesign;
+
+public interface Subject {
+	
+	public void register(Observer o);
+
+	public void unregister(Observer o);
+
+	public void notifyObserver();
+
+}
