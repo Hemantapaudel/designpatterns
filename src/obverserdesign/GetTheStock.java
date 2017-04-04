689bfa58a38a80b17c0391f2dced6ba3b726b9e0
Hemantapaudel <hemantapaudel05@gmail.com>
2017-04-05 00:30:35 +0530

    example for observer pattern

---
+++ src/obverserdesign/GetTheStock.java
@@ -0,0 +1,29 @@
+package obverserdesign;
+
+import java.text.DecimalFormat;
+
+public class GetTheStock implements Runnable {
+	private String stock;
+	private double price;
+	private Subject stockGrabber;
+
+	public GetTheStock(Subject stockGrabber, int newStartTime, String newStock,
+			double newPrice) {
+		this.stockGrabber = stockGrabber;
+		stock = newStock;
+		price = newPrice;
+	}
+
+	public void run() {
+		double randNum = (Math.random() * (.06)) - .03;
+		DecimalFormat df = new DecimalFormat("#.##");
+		price = Double.valueOf(df.format((price + randNum)));
+		if (stock == "IBM")
+			((StockGrabber) stockGrabber).setIbmPrice(price);
+		if (stock == "AAPL")
+			((StockGrabber) stockGrabber).setAaplPrice(price);
+		if (stock == "GOOG")
+			((StockGrabber) stockGrabber).setGoogPrice(price);
+	}
+
+}
---
+++ src/obverserdesign/StockGrabber.java
@@ -0,0 +1,47 @@
+package obverserdesign;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Uses the Subject interface to update all Observers
+
+public class StockGrabber implements Subject {
+
+	List<Observer> observers = new ArrayList<Observer>();
+	
+	private double ibmPrice;
+	private double aaplPrice;
+    private double googPrice;
+	
+	@Override
+	public void register(Observer observer) {
+		observers.add(observer);
+	}
+
+	@Override
+	public void unregister(Observer observer) {
+		observers.remove(observer);
+	}
+
+	@Override
+	public void notifyObserver() {
+			observers.forEach(obserser -> obserser.update(ibmPrice, aaplPrice, googPrice));
+	}
+	
+	
+	public void setIbmPrice(double ibmPrice) {
+		this.ibmPrice = ibmPrice;
+		notifyObserver();
+	}
+	
+	public void setAaplPrice(double aaplPrice) {
+		this.aaplPrice = aaplPrice;
+		notifyObserver();
+	}
+	
+	public void setGoogPrice(double googPrice) {
+		this.googPrice = googPrice;
+		notifyObserver();
+	}
+
+}
