75181c2557b711cc072dfcdc37f6fc985adffc34
Hemantapaudel <hemantapaudel05@gmail.com>
2017-05-13 02:03:57 +0530

    adding

--- src/builder/BuilderDesignpattern.java
+++ src/builder/BuilderDesignpattern.java
@@ -36,7 +36,7 @@ class Computer {
 	
 
 	//Builder Class
-	public static class ComputerBuilder{
+	static class ComputerBuilder{
 
 		// required parameters
 		private String HDD;
@@ -74,6 +74,8 @@ public class BuilderDesignpattern {
 	public static void main(String[] args) {
 		//Using builder to get the object in a single line of code and 
         //without any inconsistent state or arguments management issues		
+		
+		
         Computer comp = new Computer.ComputerBuilder(
 		"500 GB", "2 GB")
         .setBluetoothEnabled(true)
